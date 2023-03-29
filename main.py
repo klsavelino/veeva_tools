fcdd2e744c10a4d49b223d9ce02f258588acd17a
amcavel <klsavelino@gmail.com>
2023-03-29 13:29:07 -0300

    Alteração de parâmetros, correção do erro de formato de download.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from options.session import Session
 
-Session("P:\ChromeDriver\chromedriver.exe").get_report("CLM_Slides_Vault_ID_CDN")
+Session(kr_usr="USER",driver_path="P:\ChromeDriver\chromedriver.exe").get_report("REPORT")
 
 #document.querySelector(".contentArea").childNodes
 #document.querySelectorAll(".slds-brand-band_cover")[0]
--- options/session.py
+++ options/session.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome
 import keyring as kr
@@ -19,45 +20,56 @@ TIMEOUT = 20
 
 
 class Session:
-    def __init__(self, driver_path: str = DEFAULT_DRIVER_PATH, download_path: str = DOWNLOAD_PATH):
-        # Classe Session
-        # Armazena as variáveis download_path, usr, pwd e driver
+    def __init__(self, kr_usr: str, kr_addr: str = LOGIN_PAGE,  driver_path: str = DEFAULT_DRIVER_PATH, download_path: str = DOWNLOAD_PATH):
         
-        try:
-            CREDENTIALS = kr.get_credential(LOGIN_PAGE, None)
-        except:
-            raise Exception("Suas credenciais não foram devidamente armazenadas no gerenciador de credenciais do Windows.")
-
+        '''
+        # Classe "Session" #
+        Fornece base para quaisquer outros métodos que requeiram autenticação na plataforma.
+        
+        Armazena as variáveis:
+        driver: Instância chromedriver devidamente configurada para uso
+        download_path: Caminho para download de arquivos na sessão
+        '''
+        
         self.download_path = download_path
         
+        
+        try:
+            pwd = kr.get_password(kr_addr, kr_usr) # Resgata as credenciais
+        except:
+            raise Exception("""Suas credenciais não foram devidamente armazenadas no gerenciador de credenciais do Windows.""")
+            exit()
+
         # Muda diretório padrão de downloads para DOWNLOAD_PATH ou para diretório
         # especificado pelo usuário.
         
+        '''
         preferences = {'download.default_directory' : download_path,
                        'download.prompt_for_download': False
-                 }
+                 }'''
         
-        opts = Options().add_experimental_option('prefs', preferences)
-        
-
-        self.usr = CREDENTIALS.username
-        self.pwd = CREDENTIALS.password
+        opts = Options().add_experimental_option("prefs", {"devtools.download.default_directory": self.download_path})
         
         self.driver = Chrome(service=Service(executable_path=driver_path),chrome_options=opts)
+        
+        '''
+        # Automação de login #
+        Chama o chromedriver e loga no serviço com as credenciais resgatadas do keyring.
+        '''
         
         
         # Acessa a página de log-in
         (self.driver.get(LOGIN_PAGE))
         
-        # Localiza o input do e-mail
+        # Localiza o input do e-mail e envia a chave
         (WebDriverWait(self.driver, TIMEOUT)
          .until(EC.presence_of_element_located((By.ID, 'username')))
-         .send_keys(self.usr))
+         .send_keys(kr_usr))
         
-        # Localiza o input da senha
+        # Localiza o input da senha e envia a chave
         (WebDriverWait(self.driver, TIMEOUT)
          .until(EC.presence_of_element_located((By.ID, 'password')))
-         .send_keys(self.pwd))
+         .send_keys(pwd))
 
         # Clica no botão de logar
         (WebDriverWait(self.driver, TIMEOUT)
@@ -65,11 +77,18 @@ class Session:
          .click())
         
         print("Sessão iniciada")
+        
         return
     
     def get_report(self, report: str):
+        '''
+        # Método get_report(report) #
         
+        Ferramenta que resgata report usando o nome do mesmo passado como parâmetro.
         
+        Retorna PATH absoluto do local do report baixado.
+        '''
+
         (WebDriverWait(self.driver, TIMEOUT)
          .until(EC.url_contains("/lightning/page/home")))
         
@@ -130,6 +149,13 @@ class Session:
         # Retorna ao contexto original
         self.driver.switch_to.default_content()
         
+        download_behavior = {
+            "behavior": "allow",
+            "downloadPath": self.download_path
+            }
+        
+        self.driver.execute_cdp_cmd("Page.setDownloadBehavior", download_behavior)
+        
         # Aguarda presença da opção de exportação de dados
         (WebDriverWait(self.driver, TIMEOUT)
          .until(EC.presence_of_element_located((By.CSS_SELECTOR, "label[for='data-export']")))
@@ -140,17 +166,23 @@ class Session:
         (WebDriverWait(self.driver, TIMEOUT)
          .until(EC.presence_of_element_located((By.CLASS_NAME, "slds-form-element"))))
         
+        time.sleep(2)
+        
         
         # Seleciona o formato .csv
-        self.driver.execute_script("document.querySelector('.slds-select').value='localecsv';")
+        #self.driver.execute_script("document.querySelector('.slds-select').click()")
+        #self.driver.execute_script("document.querySelector('.slds-select').value='localecsv';")
+        Select(self.driver.find_element(By.CSS_SELECTOR, ".slds-select")).select_by_value("localecsv")
+        
+        
+        
+        input()
+        
         
         # Lista todos os itens no diretório de download
         before = os.listdir(self.download_path)
         
         # Baixa o report
-        self.close_all()
-        sys.exit()
-        return
     
         (WebDriverWait(self.driver, TIMEOUT)
          .until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[title='Export']")))
@@ -166,9 +198,12 @@ class Session:
         # Diferença entre os snapshots
         dir_list = list(set(after) - set(before))
         
-        for file in dir_list:
-            if file.endswith(".csv"):
-                report_path = file
+        try:
+            for file in dir_list:
+                if file.endswith(".csv"):
+                    report_path = file
+        except:
+            raise Exception("Não foi possível encontrar o arquivo criado.")
         
         # PATH do report
         report_path = os.path.join(self.download_path)
