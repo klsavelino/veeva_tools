9eaef9c0e72dddec99a30f0faa3725d1a0ef8106
amcavel <klsavelino@gmail.com>
2023-04-05 10:30:43 -0300

    Otimizações

--- main.py
+++
@@ -1,5 +0,0 @@
-from options.session import Session
-
-# Exemplo:
-    
-# Session(kr_usr="USER", kr_addr="CREDENTIALS_NAME", driver_path="CHROMEDRIVER_PATH", download_path="DOWNLOAD_PATH",).get_report("REPORT")
--- options/session.py
+++ options/session.py
@@ -12,11 +12,11 @@ import re
 import os
 
 
-
+REPORTS_PAGE = "/lightning/o/Report/home?queryScope=everything"
 LOGIN_PAGE = "https://login.salesforce.com/"
 DEFAULT_DRIVER_PATH = os.getcwd()
-DOWNLOAD_PATH = os.path.join(os.path.expanduser("~"), "AppData", "Local", "Temp", "TEMPDIR")
-TIMEOUT = 30
+DEFAULT_DOWNLOAD_PATH = os.path.join(os.path.expanduser("~"), "AppData", "Local", "Temp", "TEMPDIR")
+DEFAULT_TIMEOUT = 30
 
 
 class Session:
@@ -24,7 +24,7 @@ class Session:
                  kr_usr: str, # Nome do username no Keyring
                  kr_addr: str = LOGIN_PAGE, # Nome das credenciais no Keyring
                  driver_path: str = DEFAULT_DRIVER_PATH, # Caminho para o chromedriver.exe
-                 download_path: str = DOWNLOAD_PATH): # Caminho desejado para download do report
+                 download_path: str = DEFAULT_DOWNLOAD_PATH): # Caminho desejado para download do report
         
         '''
         # Classe "Session" #
@@ -66,26 +66,30 @@ class Session:
         # Acessa a página de log-in
         (self.driver.get(LOGIN_PAGE))
         
-        # Localiza o input do e-mail e envia a chave
-        (WebDriverWait(self.driver, TIMEOUT)
+        
+        # Localiza o input do e-mail e envia a credencial
+        (self._element_wait()
          .until(EC.presence_of_element_located((By.ID, 'username')))
          .send_keys(kr_usr))
         
-        # Localiza o input da senha e envia a chave
-        (WebDriverWait(self.driver, TIMEOUT)
+        # Localiza o input da senha e envia a credencial
+        (self._element_wait()
          .until(EC.presence_of_element_located((By.ID, 'password')))
          .send_keys(pwd))
 
         # Clica no botão de logar
-        (WebDriverWait(self.driver, TIMEOUT)
+        (self._element_wait()
          .until(EC.presence_of_element_located((By.ID, 'Login')))
          .click())
+        
+        self._page_wait()
         
         print("Sessão iniciada")
         
         return
 
     def get_report(self, report: str):
+        
         '''
         # Método get_report(report) #
         
@@ -95,11 +99,8 @@ class Session:
         '''
         
         # Aguarda página home carregar
-        (WebDriverWait(self.driver, TIMEOUT)
-         .until(EC.url_contains("/lightning/page/home")))
-        
-        REPORTS_PAGE = "/lightning/o/Report/home?queryScope=everything"
-        
+        #(WebDriverWait(self.driver, TIMEOUT)
+        # .until(EC.url_contains("/lightning/page/home")))
         
         url = self.driver.current_url
         url = url[:(url.find(".com")+4)]
@@ -110,39 +111,52 @@ class Session:
     
         self.driver.get(BASE_URL)
         
+        self._page_wait()
+        
         # Aguarda presença de input de texto onde irá ser inserido o report para a pesquisa
-        (WebDriverWait(self.driver, TIMEOUT)
+        (self._element_wait()
          .until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='text']")))
          .send_keys(report))
         
-        time.sleep(5)
+
+        self._page_wait()
+        
+        self._element_wait().until(lambda frame: self.driver.execute_script("return document.querySelector('.folderListView') ==  null") == False)
+        
+        time.sleep(10)
         
         try:
             #source = self.driver.page_source()
             source = self.driver.execute_script("return document.querySelector('.bodyContainer').innerHTML")
             assert(source is not None)
+            
+        except AssertionError:
+            print("Não foi possível acessar o innerHTML para a consulta.")
+            raise
+            
+        try:
             regex = r'<a href="(?:[^\\"]|\\\\|\\")*" title=\"' + report
             result = re.search(regex, source, re.MULTILINE)
             assert(result is not None)
-
+        
         except AssertionError:
-            print("Não foi possível acessar o report.")
+            print("Não foi possível acessar referência do report via regex.")
             raise
         
         result = result.group(0).split('"')[1] # Retorna apenas link
         
         self.driver.get(result)
         
-        time.sleep(5)
+        self._page_wait()
         
         try:
             
             iframe_element = self.driver.execute_script('return document.querySelector(".isView")')
             
-            iframe_presence = False if iframe_element == None else True
+            iframe_presence = False if iframe_element == None else True        
             
             if iframe_presence:
-                iframe = (WebDriverWait(self.driver, TIMEOUT)
+                iframe = (self._element_wait()
                           .until(EC.presence_of_element_located((By.CSS_SELECTOR, ".isView"))))
                 self.driver.switch_to.frame(iframe)
                 print("Sucesso, o elemento iframe foi localizado")
@@ -152,13 +166,13 @@ class Session:
             raise
         
         # Exibe as opções do dropdown
-        (WebDriverWait(self.driver, TIMEOUT)
+        (self._element_wait()
         .until(EC.presence_of_element_located((By.CSS_SELECTOR, ".slds-dropdown-trigger_click")))
         .click())
         
         # Aguarda presença e clica na opção "Exportar" no dropdown
         
-        (WebDriverWait(self.driver, TIMEOUT)
+        (self._element_wait()
          .until(EC.presence_of_element_located((By.CSS_SELECTOR, ".report-action-ReportExportAction")))).find_element(By.CSS_SELECTOR, "*").click()
         
         
@@ -176,34 +190,41 @@ class Session:
         self.driver.execute_cdp_cmd("Page.setDownloadBehavior", download_behavior)
         
         # Aguarda presença da opção de exportação de dados
-        (WebDriverWait(self.driver, TIMEOUT)
+        (self._element_wait()
          .until(EC.presence_of_element_located((By.CSS_SELECTOR, "label[for='data-export']")))
          .click())
     
         
         # Aguarda a presença da opção do formato dos dados a serem exportados
-        (WebDriverWait(self.driver, TIMEOUT)
+        (self._element_wait()
          .until(EC.presence_of_element_located((By.CLASS_NAME, "slds-form-element"))))
-        
-        time.sleep(2)
         
         
         # Seleciona o formato .csv
+        (self._element_wait()
+         .until(EC.presence_of_element_located((By.CSS_SELECTOR, ".slds-select"))))
 
         Select(self.driver.find_element(By.CSS_SELECTOR, ".slds-select")).select_by_value("localecsv")
+        
+        '''
+        INSERIR FUNÇÃO DE ACOMPANHAMENTO DE PROGRESSO DE REPORT
+        '''
         
         
         # Lista todos os itens no diretório de download
         before = os.listdir(self.download_path)
         
+        input()
+        self._close_all()
+        
         # Baixa o report
-        (WebDriverWait(self.driver, TIMEOUT)
+        (self._element_wait()
          .until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uiButton--brand")))
          .click())
                      
         
         # Tempo de espera
-        time.sleep(TIMEOUT)
+        time.sleep(DEFAULT_TIMEOUT)
         
         # Lista todos os itens no diretório de download
         after = os.listdir(self.download_path)
@@ -215,8 +236,10 @@ class Session:
             for file in dir_list:
                 if file.endswith(".csv"):
                     report_path = file
+                    del file
+        
         except:
-            raise Exception("Não foi possível encontrar o arquivo criado.")
+            raise
         
         # PATH do report
         report_path = os.path.join(self.download_path, report_path)
@@ -224,10 +247,18 @@ class Session:
         print(report_path)
         
         return report_path
-        
-    def close_all(self):
+    
+    def _element_wait(self, timeout = DEFAULT_TIMEOUT):
+        return WebDriverWait(self.driver, timeout)
+    
+    def _page_wait(self, timeout = DEFAULT_TIMEOUT):
+        return self._element_wait(timeout).until(lambda driver: self.driver.execute_script("return document.readyState")=='complete')
+    
+
+    def _close_all(self):
         self.driver.close()
         self.driver.quit()
+        sys.exit()
 
                       
 
